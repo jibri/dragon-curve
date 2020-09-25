b8438f1e9eb157e2b85a98657c265667fd4aed96
Jibri <jeremie.briand@outlook.fr>
2020-09-25 13:41:19 +0200

    init firebase

--- dragonCurve.js
+++ dragonCurve.js
@@ -1,162 +1,169 @@
 // CONSTANTES
-const startX = 500
-const startY = 680
-const lineBaseLength = 1000
-let cloudEffect = false
-let stop = false
-let animate = false
-let iterationsAsk = 1
+const startX = 500;
+const startY = 680;
+const lineBaseLength = 1000;
+let cloudEffect = false;
+let stop = false;
+let animate = false;
+let iterationsAsk = 1;
 
 // Canvas
-let canvas = document.getElementById("myCanvas")
-let ctx = canvas.getContext("2d")
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
 
 // var grad = ctx.createRadialGradient(startX, startY, 50, 750, startY, 750);
 // grad.addColorStop(0, 'rgb(200,230,255)');
 // grad.addColorStop(1, 'rgb(0,80,255)');
-ctx.strokeStyle = '#05FF05';
+ctx.strokeStyle = "#05FF05";
+ctx.fillStyle = "#444444";
 // ctx.fillRect(0, 0, 2000, 1500);
 
-ctx.lineWidth = 5
+ctx.lineWidth = 5;
 
 // Récupération des propriétés de la dragon curve
 const getCurve = (iterations) => {
-    const curve = []
+  const curve = [];
 
-    for (let i = 1; i <= iterations; i++) {
-        let size = curve.length
-        curve.push(1)
-        for (let j = (size - 1); j >= 0; j--) {
-            curve.push(-curve[j])
-        }
+  for (let i = 1; i <= iterations; i++) {
+    let size = curve.length;
+    curve.push(1);
+    for (let j = size - 1; j >= 0; j--) {
+      curve.push(-curve[j]);
     }
-    return curve
-}
+  }
+  return curve;
+};
 
 const draw = (iterations, isCircle) => {
+  stop = false;
 
-    stop = false;
+  // Use the identity matrix while clearing the canvas
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.translate(startX, startY);
+  ctx.rotate((-iterations * Math.PI) / 4);
 
-    // Use the identity matrix while clearing the canvas
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-    //ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.translate(startX, startY)
-    ctx.rotate(-iterations * Math.PI / 4)
+  const curve = getCurve(iterations);
+  let lineSize = lineBaseLength / Math.pow(2, iterations / 2);
+  if (isCircle) lineSize /= 2;
 
-    const curve = getCurve(iterations)
-    let lineSize = lineBaseLength / Math.pow(2, iterations / 2)
-    if (isCircle) lineSize /= 2
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(lineSize, 0);
+  ctx.translate(lineSize, 0);
 
-    ctx.beginPath()
-    ctx.moveTo(0, 0)
-    ctx.lineTo(lineSize, 0)
-    ctx.translate(lineSize, 0)
-
-    if (isCircle) drawCurves(curve, lineSize, 0)()
-    else drawLines(curve, lineSize, 0)();
-}
+  if (isCircle) drawCurves(curve, lineSize, 0)();
+  else drawLines(curve, lineSize, 0)();
+};
 
 const drawLines = (curve, lineSize, index) => () => {
+  if (index >= curve.length) {
+    ctx.stroke();
+    return;
+  }
 
-    if (index >= curve.length) {
-        ctx.stroke()
-        return
-    }
-
-    const batchLength = Math.ceil(curve.length / 60)
-    const nextStep = Math.min(curve.length, index + batchLength)
-    for (let i = index; i < nextStep; i++) {
-        ctx.moveTo(0, 0)
-        if (curve[i] === 1) ctx.rotate(Math.PI / 2)
-        else ctx.rotate(-Math.PI / 2)
-        ctx.lineTo(lineSize, 0)
-        ctx.translate(lineSize, 0)
-    }
-    drawNext(animate, drawLines(curve, lineSize, nextStep))
-}
+  const batchLength = Math.ceil(curve.length / 60);
+  const nextStep = Math.min(curve.length, index + batchLength);
+  for (let i = index; i < nextStep; i++) {
+    ctx.moveTo(0, 0);
+    if (curve[i] === 1) ctx.rotate(Math.PI / 2);
+    else ctx.rotate(-Math.PI / 2);
+    ctx.lineTo(lineSize, 0);
+    ctx.translate(lineSize, 0);
+  }
+  drawNext(animate, drawLines(curve, lineSize, nextStep));
+};
 
 const drawCurves = (curve, radius, index) => () => {
+  if (index >= curve.length) {
+    ctx.lineTo(radius, 0);
+    ctx.stroke();
+    return;
+  }
 
-    if (index >= curve.length) {
-        ctx.lineTo(radius, 0)
-        ctx.stroke()
-        return
+  const batchLength = Math.ceil(curve.length / 60);
+  const nextStep = Math.min(curve.length, index + batchLength);
+  for (let i = index; i < nextStep; i++) {
+    // -------------------- CLOUD EFFET
+    if (cloudEffect) {
+      const red = 150 * (i / curve.length);
+      const green = 80 + (120 * i) / curve.length;
+      ctx.strokeStyle = `rgb(${red},${green},255)`;
+
+      // const h = 240 - 30 * (i / curve.length)
+      // const l = 50 + 30 * (i / curve.length)
+
+      // const h = (Math.cos(-Math.PI + i / 20000) + 1) * 15 + 210
+      // const l = (Math.cos(Math.PI / 2 + i / 20000) + 1) * 15 + 50
+      // TOP bleu clair violet clair
+      const h = (Math.cos(-Math.PI / 2 + i / 20000) + 1) * 15 + 210;
+      const l = (Math.sin(Math.PI / 2 + i / 20000) + 1) * 15 + 50;
+      ctx.strokeStyle = `hsl(${h},100%,${l}%)`;
+      ctx.stroke();
+      ctx.beginPath();
     }
+    // -------------------- CLOUD EFFET
 
-    const batchLength = Math.ceil(curve.length / 60)
-    const nextStep = Math.min(curve.length, index + batchLength)
-    for (let i = index; i < nextStep; i++) {
-
-        // -------------------- CLOUD EFFET
-        if (cloudEffect) {
-            const red = 200 * (i / curve.length)
-            const green = 80 + (150 * i / curve.length)
-            ctx.strokeStyle = `rgb(${red},${green},255)`
-            ctx.stroke()
-            ctx.beginPath()
-        }
-        // -------------------- CLOUD EFFET
-
-        ctx.moveTo(0, 0)
-        if (curve[i] === 1) {
-            ctx.arc(0, radius, radius, -Math.PI / 2, 0, false)
-            ctx.translate(radius, radius)
-            ctx.rotate(Math.PI / 2)
-        } else {
-            ctx.arc(0, -radius, radius, +Math.PI / 2, 0, true)
-            ctx.translate(radius, -radius)
-            ctx.rotate(-Math.PI / 2)
-        }
+    ctx.moveTo(0, 0);
+    if (curve[i] === 1) {
+      ctx.arc(0, radius, radius, -Math.PI / 2, 0, false);
+      ctx.translate(radius, radius);
+      ctx.rotate(Math.PI / 2);
+    } else {
+      ctx.arc(0, -radius, radius, +Math.PI / 2, 0, true);
+      ctx.translate(radius, -radius);
+      ctx.rotate(-Math.PI / 2);
     }
-    drawNext(animate, drawCurves(curve, radius, nextStep))
-}
+  }
+  drawNext(animate, drawCurves(curve, radius, nextStep));
+};
 
 const drawNext = (animate, drawFunction) => {
-    if (stop) return
-    if (animate) {
-        ctx.stroke()
-        window.requestAnimationFrame(drawFunction);
-    } else {
-        drawFunction()
-    }
-}
+  if (stop) return;
+  if (animate) {
+    ctx.stroke();
+    window.requestAnimationFrame(drawFunction);
+  } else {
+    drawFunction();
+  }
+};
 
 const plusL = () => {
-    draw(++iterationsAsk, false)
-    updateLevel(iterationsAsk)
-}
+  draw(++iterationsAsk, false);
+  updateLevel(iterationsAsk);
+};
 const moinsL = () => {
-    draw(--iterationsAsk, false)
-    updateLevel(iterationsAsk)
-}
+  draw(--iterationsAsk, false);
+  updateLevel(iterationsAsk);
+};
 const plusC = () => {
-    draw(++iterationsAsk, true)
-    updateLevel(iterationsAsk)
-}
+  draw(++iterationsAsk, true);
+  updateLevel(iterationsAsk);
+};
 const moinsC = () => {
-    draw(--iterationsAsk, true)
-    updateLevel(iterationsAsk)
-}
+  draw(--iterationsAsk, true);
+  updateLevel(iterationsAsk);
+};
 const updateLevel = (newLevel) => {
-    // Affichage du niveau
-    const niveau = document.getElementById("niveau")
-    niveau.innerText = `Niveau : ${newLevel}`
-}
+  // Affichage du niveau
+  const niveau = document.getElementById("niveau");
+  niveau.innerText = `Niveau : ${newLevel}`;
+};
 const changeAnimate = (event) => {
-    animate = event.target.checked
-}
+  animate = event.target.checked;
+};
 const setStop = () => {
-    stop = true
+  stop = true;
 
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
 
 // Affichage du niveau
-updateLevel(1)
+updateLevel(18);
 // Affichage de la courbe N1
-cloudEffect = true
-draw(16, true)
+cloudEffect = true;
+draw(18, true);
 // cloudEffect = false
 // ctx.strokeStyle = '#05AA05';
 // ctx.lineWidth = 2;
